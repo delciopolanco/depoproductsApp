b7d721a8e6f9b3ca7c39970b1c982c7df4dfbbd6
Delcio Polanco <delciopolanco@gmail.com>
2023-11-18 18:50:20 -0400

    fix

--- src/shared/services/axios.tsx
+++ src/shared/services/axios.tsx
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const instance = axios.create({
 	headers: {
-		'Access-Control-Allow-Origin': '*'
+		'Access-Control-Allow-Origin': '*',
+		Accept: 'application/json'
 	}
 });
 
